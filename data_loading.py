f5b0cbbb7aa38cbac9bc6fb301cc8321f512f911
antoncrd <anton.conrad.0505@gmail.com>
2025-09-01 13:54:05 +0200

    oups

--- data_loading.py
+++ data_loading.py
@@ -7,13 +7,13 @@ def run_cmd(cmd: str):
 
 def main():
     # 1. Récupérer les données
-    run_cmd("python get_data.py")
+    run_cmd("python data_loading.utils/get_data.py")
 
     # 2. Transformer les JSON en CSV
-    run_cmd("python all_json_to_csv.py data/data_json/ data/data_csv/")
+    run_cmd("python data_loading.utils/all_json_to_csv.py data/data_json/ data/data_csv/")
 
     # 3. Agréger les CSV par module
-    run_cmd("python aggregate_csv.py --indir data/data_csv/ --outdir data/")
+    run_cmd("python data_loading.utils/aggregate_csv.py --indir data/data_csv/ --outdir data/")
 
 if __name__ == "__main__":
     main()
